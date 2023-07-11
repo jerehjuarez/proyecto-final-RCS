45bc4d44800f0f14b13c24151b8ef1cfed5a0fdc
francojnieva <franco97nieva@hotmail.com>
2023-07-11 18:17:34 -0300

    feat: add react-bootstrap bootstrap dependencies, add footer component and clear content in App.css and index.css

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        {/* ACÁ VA EL CONTENIDO DEL FOOTER */} 
+    </div>
+  )
+}
+
+export default Footer
