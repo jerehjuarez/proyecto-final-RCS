88b0d1fdf7fdfa0ac9cadc1d31546584fa825df7
francojnieva <franco97nieva@hotmail.com>
2023-07-13 12:24:15 -0300

    feat: change the content on the error page and route to the home page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ErrorPage() {
   return (
     <div>
-    <h1 className='text-center'>Acá va el contenido cuando el usuario pone una dirección en la barra de búsqueda que no es correcta</h1>
+      <h1 className='text-center'>404</h1>
+      <h2 className='text-center fs-3'>Página no encontrada</h2>
+      <div className='text-center'>
+      <Link to="/" className='btn btn-primary m-3'>Ir al Inicio <i className="bi bi-house-door-fill"></i></Link>
+      </div>
   </div>
   )
 }
