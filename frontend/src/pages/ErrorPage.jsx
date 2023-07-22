f1e0c1ac51fb5f66edd7f2df86aba5fff31123c3
Belén Gómez <gomezbelen86@gmail.com>
2023-07-22 17:39:24 -0300

    feat:add style to error page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom'
 
 function ErrorPage() {
   return (
-    <div>
-      <h1 className='text-center'>404</h1>
+    <div className='page-error-container'>
+    
+      {/*<h1 className='text-center'>404</h1>*/}
+      <div class="loader">
+  <p className="text-center text-white">
+  <span className="letter letter1">4</span>
+  <span className="letter letter2">0</span>
+  <span className="letter letter3">4</span>
+  </p>
+</div>
       <h2 className='text-center fs-3'>Página no encontrada</h2>
       <div className='text-center'>
       <Link to="/" className='btn btn-primary m-3'>Ir al Inicio <i className="bi bi-house-door-fill"></i></Link>
