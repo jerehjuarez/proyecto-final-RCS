274083979275275ffb5543b010f5e37f738e814d
francojnieva <franco97nieva@hotmail.com>
2023-07-22 21:43:25 -0300

    feat: change background color and border in elememts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,54 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-function App() {
-  const [textInput, setTextInput] = useState('');
-  const [taskList, setTaskList] = useState([]);
-
-  const handleInputChange = (event) => {
-    setTextInput(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    if (textInput.trim() === '') {
-      return;
-    }
-
-    const newTask = {
-      id: Date.now(),
-      text: textInput
-    };
-
-    setTaskList([...taskList, newTask]);
-    setTextInput('');
-  };
-
-  const handleDeleteTask = (taskId) => {
-    setTaskList(taskList.filter(task => task.id !== taskId));
-  };
-
+function Home() {
   return (
-    <div className='content'>
-      <h1 className='title'>Lista de tareas</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          value={textInput}
-          onChange={handleInputChange}
-          placeholder="Añadir tarea..."
-        />
-        <input type="submit" value="Añadir" />
-      </form>
-      <ul className='list'>
-        {taskList.map(task => (
-          <li className='text' key={task.id}>
-            <button className='delete' onClick={() => handleDeleteTask(task.id)}>✓</button>
-            {task.text}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
 
-export default App;
+export default Home
--- frontend/src/pages/ToDoLists.jsx
+++ frontend/src/pages/ToDoLists.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import { useState } from 'react';
 
 function ToDoLists() {
+  const [textInput, setTextInput] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const handleInputChange = (event) => {
+    setTextInput(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (textInput.trim() === '') {
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      text: textInput
+    };
+
+    setTaskList([...taskList, newTask]);
+    setTextInput('');
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTaskList(taskList.filter(task => task.id !== taskId));
+  };
+
   return (
-    <div>
-        <h1>Acá va la lista de tareas</h1>
+    <div className='content'>
+      <h1 className='title'>Lista de tareas</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={textInput}
+          onChange={handleInputChange}
+          placeholder="Añadir tarea..."
+        />
+        <input type="submit" value="Añadir" />
+      </form>
+      <ul className='list'>
+        {taskList.map(task => (
+          <li className='text' key={task.id}>
+            <button className='delete' onClick={() => handleDeleteTask(task.id)}>✓</button>
+            {task.text}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default ToDoLists
